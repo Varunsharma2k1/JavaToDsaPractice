46cda3d2f9f02e1e5787063c0289ed4784fd0004
craps <craps@DESKTOP-NT0HBT3>
2022-08-02 23:24:34 +0530

    update

--- src/com/Varun/Equilibrium.java
+++ src/com/Varun/Equilibrium.java
@@ -2,6 +2,20 @@ package com.Varun;
 
 public class Equilibrium {
     public static void main(String[] args) {
+ int[] arr ={1, 2, 3};
+    int sum=0;
+    for(int i=1;i<arr.length;i++){
+        sum+=arr[i-1];
+        int leftsum=0;
+      for(int j=i+1;j<arr.length;j++){
 
-    }
+          leftsum+=arr[j];
+      }
+      if(leftsum==sum){
+          System.out.println(i);
+          return;
+    }}
+    
 }
+}
+
